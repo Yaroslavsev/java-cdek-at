a94366e335d205e96539280f3550a5f31bb9a495
Anton Yaroslavsev <yaroslavsev@mail.ru>
2023-07-20 15:02:01 +0500

    live_coding_17_07_2023 StreamAPI v2

--- src/main/java/org/example/live_coding_17_07_2023/Main.java
+++ src/main/java/org/example/live_coding_17_07_2023/Main.java
@@ -1,8 +1,6 @@
 package org.example.live_coding_17_07_2023;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -25,7 +23,7 @@ public class Main {
         persons.add(new Person("Victor", 95));
 
 
-        /*System.out.println(mercenaries.stream().filter(mercenary -> mercenary.getAge() > 20).count());
+        System.out.println(mercenaries.stream().filter(mercenary -> mercenary.getAge() > 20).count());
         System.out.println(mercenaries
                 .stream()
                 .filter(m -> m.getSpecialization().equals(SpecializationEnum.ARTILLERIST))
@@ -45,7 +43,7 @@ public class Main {
                 .stream()
                 .filter(m -> m.getSpecialization().equals(SpecializationEnum.ARTILLERIST))
                 .map(merc -> merc.getSalary())
-                .forEach(cont -> System.out.println(cont));*/
+                .forEach(cont -> System.out.println(cont));
 
         System.out.println(" ");
         System.out.println(" ");
@@ -53,13 +51,13 @@ public class Main {
         System.out.println("Всех наемников в алфавитном порядке, у которых специализация не командир, убавить им зп на 1000 и вывести на консоль");
         System.out.println(" ");
 
-        List<Mercenary> mercsExercise1 = mercenaries
+        List<Mercenary> mercsNaturalNonCommanderWaithSalaryMinus1000 = mercenaries
                 .stream()
                 .filter(merc -> !merc.getSpecialization().equals(SpecializationEnum.COMMANDOR))
                 .sorted(Comparator.comparing(Mercenary::getName))
                 .map(merc -> new Mercenary(merc.getName(), merc.getAge(), merc.getSpecialization(), merc.getSalary() - 1000))
                 .collect(Collectors.toList());
-        System.out.println(mercsExercise1);
+        System.out.println(mercsNaturalNonCommanderWaithSalaryMinus1000);
 
         System.out.println(" ");
         System.out.println(" ");
@@ -67,22 +65,56 @@ public class Main {
         System.out.println("Вывести на экран пары Наемник-Человек, где ключом будет общее имя");
         System.out.println(" ");
 
-        List<Mercenary> resultMercenariesExercise2 = mercenaries
+        List<Mercenary> mercsWithEqualNameAsPers = mercenaries
                 .stream()
                 .filter(pers -> persons.stream().anyMatch(merc -> merc.getName().equals(pers.getName())))
                 .collect(Collectors.toList());
-        System.out.println(resultMercenariesExercise2);
 
-        List<Person> resultPersonsExercise2 = persons
+        List<Person> persWithEqualNameAsMercs = persons
                 .stream()
                 .filter(merc -> mercenaries.stream().anyMatch(pers -> pers.getName().equals(merc.getName())))
                 .collect(Collectors.toList());
-        System.out.println(resultPersonsExercise2);
+
+        HashMap<Mercenary, Person> mapEqualNamesMercAndPers = new HashMap<>();
+        for (int i = 0; i < mercsWithEqualNameAsPers.size(); i++) {
+            mapEqualNamesMercAndPers.put(mercsWithEqualNameAsPers.get(i), persWithEqualNameAsMercs.get(i));
+        }
+        System.out.println(mapEqualNamesMercAndPers);
 
         System.out.println(" ");
         System.out.println(" ");
         System.out.println("-------------------------------------ЗАДАНИЕ 3-------------------------------------------");
         System.out.println("Сгруппировать наемников по величине зп и вывести на экран отдельно (от 1 до 1000, от 1000 до 10000, от 17000)");
+        System.out.println(" ");
+
+        List<Mercenary> mercsSalaryFrom1to1k = mercenaries
+                .stream()
+                .filter(merc -> merc.getSalary() >= 1 && merc.getSalary() <= 1000)
+                .collect(Collectors.toList());
+
+        HashMap<String, List<Mercenary>> mapFrom1to1k = new HashMap<>();
+        mapFrom1to1k.put("Mercenaries with salary from 1 to 1 000\n", mercsSalaryFrom1to1k);
+        System.out.println(mapFrom1to1k);
+        System.out.println(" ");
+
+        List<Mercenary> mercsSalaryFrom1kTo10k = mercenaries
+                .stream()
+                .filter(merc -> merc.getSalary() > 1000 && merc.getSalary() <= 10000)
+                .collect(Collectors.toList());
+
+        HashMap<String, List<Mercenary>> mapFrom1kTo10k = new HashMap<>();
+        mapFrom1kTo10k.put("Mercenaries with salary from 1 000 to 10 000\n", mercsSalaryFrom1kTo10k);
+        System.out.println(mapFrom1kTo10k);
+        System.out.println(" ");
+
+        List<Mercenary> mercsSalaryFrom17k = mercenaries
+                .stream()
+                .filter(merc -> merc.getSalary() >= 17000)
+                .collect(Collectors.toList());
+
+        HashMap<String, List<Mercenary>> mapFrom17k = new HashMap<>();
+        mapFrom17k.put("Mercenaries with salary from 17 000\n", mercsSalaryFrom17k);
+        System.out.println(mapFrom17k);
         System.out.println(" ");
     }
 }
